15a69f309b6ae19426ae40f44691ed1a5953fa08
NakhunSong <nak4627@gmail.com>
2023-06-04 16:50:23 +0900

    feat: set router by gorilla/mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module practice/go-web-server
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
 	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 
-	http.ListenAndServe(":8080", nil)
+	productRouter := router.PathPrefix("/products").Subrouter()
+
+	productRouter.HandleFunc("/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the product: %s on page %s\n", title, page)
+	})
+
+	http.ListenAndServe(":8080", router)
 }
